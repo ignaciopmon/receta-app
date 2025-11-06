0702edbd210321688292a7cae3c592965ea10541
ignaciopmon <ignaciopmonroy@gmail.com>
2025-11-06 20:01:25 +0100

    Cambio para nunca guardar caché de share

--- app/share/[id]/page.tsx
+++ app/share/[id]/page.tsx
@@ -8,6 +8,8 @@ import Link from "next/link"
 import Image from "next/image"
 import { SaveRecipeButton } from "@/components/save-recipe-button"
 
+export const revalidate = 0;
+
 // Esta es una cabecera simple para la página pública
 function ShareHeader() {
   return (
